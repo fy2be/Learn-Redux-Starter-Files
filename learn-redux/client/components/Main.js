f3c8be236c31f02800d7e5d768a3c28d89772f38
fy2be <36625528+fy2be@users.noreply.github.com>
2018-06-17 13:57:49 +0200

    L3

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = () => (
+const Main = (props) => (
     <div>
         <h1>
             <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(props.children, props)}
     </div>
 );
 
