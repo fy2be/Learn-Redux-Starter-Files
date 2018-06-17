a0cb09929a1b401aa11335913270e17f98492002
fy2be <36625528+fy2be@users.noreply.github.com>
2018-06-17 23:00:46 +0200

    L14

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -4,13 +4,15 @@ import Comments from './Comments';
 
 const Single = (props) => {
     // index of the post
-    const i = props.posts.findIndex((post) => post.code === props.params.postId);
+    const { postId } = props.params;
+    const i = props.posts.findIndex((post) => post.code === postId);
     const post = props.posts[i];
+    const postComments = props.comments[postId];
 
     return (
         <div className="single-photo">
             <Photo i={i} post={post} {...props} />
-            <Comments />
+            <Comments postComments={postComments} />
         </div>
     );
 };
